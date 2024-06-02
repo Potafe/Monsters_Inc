08221805b24e244fd73c5aabc7f07594e5198681
Yazat <bt21me011@nitmz.ac.in>
2024-06-02 22:49:35 +0530

    feat: add alien module

--- game/Monster_Invasion.py
+++ game/Monster_Invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from Modules.settings import Settings
 from Modules.Ship import Ship
+from Modules.Alien import Alien
 
 import Modules.game_function as gf
 
@@ -16,13 +17,17 @@ def run_game():
     
     pygame.display.set_caption("Monster Invasion")
 
+    alien = Alien(ai_settings, screen)
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
 
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)        
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
